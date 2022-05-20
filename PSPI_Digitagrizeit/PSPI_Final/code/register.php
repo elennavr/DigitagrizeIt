72e0c37536500048ea91b436ac5c09c4ca068e40
Eleni Navrozidou <72340658+elennavr@users.noreply.github.com>
2022-05-20 14:52:01 +0300

    rename from .html->.php, checks if email is okay

--- PSPI_Digitagrizeit/PSPI_Final/code/register.php
+++ PSPI_Digitagrizeit/PSPI_Final/code/register.php
@@ -1,3 +1,71 @@
+<?php
+    
+$showAlert = false; 
+$showError = false; 
+$exists=false;
+$flag_user = false;
+$flag_psd= false;
+
+    
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+      
+    // Include file which makes the
+    // Database Connection.
+    include 'connect.php';   
+    
+    $username = $_POST["username"]; 
+    $password1 = $_POST["password1"]; 
+    $password2 = $_POST["password2"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $address = $_POST["address"];
+    $email = $_POST["email"];
+    $phone_num = $_POST["phone_num"];
+    
+    $sql = "Select * from users where username='$username'";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    $num = mysqli_num_rows($result); 
+    if(!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email)){
+      $msg = 'The email you have entered is invalid, please try again.';
+      $corr_mail = false;
+    }else{
+      if(!$flag_user){
+        $corr_mail = true;
+      }  
+    } 
+
+    if($num == 0 && $corr_mail) {
+        if(($password1 == $password2) && $exists==false) {
+    
+            $hash = password_hash($password1, 
+                                PASSWORD_DEFAULT);
+            
+            // Password Hashing is used here. 
+            $sql = "INSERT INTO users (username, password, email, first_name, last_name, address, phone_num) 
+                VALUES ('$username', '$hash', '$email','$first_name', '$last_name', '$address', '$phone_num');";
+    
+            $result = mysqli_query($conn, $sql);
+    
+            if ($result) {
+              $msg = 'Your account has been made.'; 
+              
+             }else { 
+              $msg = "Invalid Password."; 
+            }      
+          } 
+        }
+    
+   if($num>0 && $corr_mail){
+      $msg = "Username not available"; 
+   } 
+
+  
+}
+    
+    
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -14,11 +82,12 @@
   </head>
   <body>
     <div class="tab">
-    <div> <br>
-      <a id="nextTo" href="login.html" class="previous round" title="Login screen">&#8249;</a> 
       <h2 align="center">Sign Up</h2>
-      <br></div>
-     
+      <?php 
+      if(isset($msg)){  // Check if $msg is not empty
+          echo '<h2 align="center">'.$msg.'</h2>'; // Display our message and wrap it with a div with the class "statusmsg".
+      } 
+      ?>
       <form action="register.php" method="post">
         <h4 class="centerCrec">Username</h4>
         <input
