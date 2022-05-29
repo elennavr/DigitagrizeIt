44c73e940ee7992a817203dc184c77fd82ab2df5
Eleni Navrozidou <72340658+elennavr@users.noreply.github.com>
2022-05-29 20:11:42 +0300

    Update UserProfile.php

--- a.FINAL FILES/UserProfile.php
+++ a.FINAL FILES/UserProfile.php
@@ -7,6 +7,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+ <title>Digitagrize it - User Profile</title>
+ <link rel="icon" href="images/icons/favicon.png" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="breadcrumb.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
